4b33689fa90236ff95361c75a8ab9103ec3dbe31
Aman Deol <amandeolm@gmail.com>
2024-09-04 01:01:25 +0530

    added animations

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -2,13 +2,17 @@
 interface EventProps {
   className?: string;
   onClick?: () => void;
+  style?: React.CSSProperties;
 }
 
-export default function Event({ className, onClick }: EventProps) {
+export default function Event({ className, onClick, style}: EventProps) {
   return (
     <div
       onClick={onClick}
       className={`rounded-xl w-[23rem] text-white px-5 py-3 font-bold ${className}`}
+      style={{
+        transition: "background-color 0.3s ease-in-out", 
+      }}
     >
       <h1 className="text-base">EVENT 1 : iOS FUSION INTRO</h1>
       <p className="text-xs text-[#9f82f1]">9:00AM - 10:30AM</p>
