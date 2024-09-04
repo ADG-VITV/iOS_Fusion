1dd7d63fc00fe44b55c442f6b7ad892313b7e173
Aman Deol <amandeolm@gmail.com>
2024-09-04 23:55:28 +0530

    insignifacant changes

--- src/sections/Description.tsx
+++ src/sections/Description.tsx
@@ -10,7 +10,7 @@ export default function Description() {
           </p>
           <p className="text-gray-400">CREATE.IDEATE.INNOVATE</p>
         </div>
-        <div className="w-[90%] m-4 sm:m-auto sm:w-[75%] sm:h-[60%] bg-[#1A1A1A] text-white sm:p-16 text-center rounded-xl">
+        <div className="w-[90%] m-4 sm:m-auto sm:w-[75%] sm:h-[60%] bg-[#1A1A1A] text-white sm:p-16 text-center rounded-xl ">
           <div className=" flex-col hidden sm:flex items-center">
             <p className="sm:text-4xl ">
               <span className="text-[#5F2EEA]">iOS</span> FUSION
@@ -23,7 +23,7 @@ export default function Description() {
           <p className="m-4">
             The journey begins with a hands-on workshop that covers the essentials of Swift, Apple's powerful programming language. Participants will learn the ropes of app development from experienced mentors, gaining practical knowledge that can be immediately applied. 
           </p>
-          <p className="m-4">
+          <p className="m-4 ">
             But the excitement doesn’t end there. The event culminates in a thrilling Appathon, where participants will race against the clock to create the most innovative iOS apps, putting their newfound skills to the ultimate test. With its blend of learning, creativity, and competition, IOS Fusion 7.0 is your gateway to pushing the limits of technology and innovation. Don’t miss out on this electrifying experience that could be the start of your journey as a top-tier app developer.
           </p>
         </div>
--- src/sections/FAQ.tsx
+++ src/sections/FAQ.tsx
@@ -5,12 +5,12 @@ export default function FAQ() {
     <section id="faq" className=" bg-black text-white md:px-32 md:py-20 h-full p-10">
       <h1 className="text-5xl font-bold m-4">FAQ</h1>
       <div className="flex flex-col gap-8 m-4">
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
+        <Question ques='1. What is the team size for the Appathon?'  ans='1-3'/>
+        <Question ques ='2. Do we need any prerequisite knowledge?' ans='No, but it’s great if you know a bit'/>
+        <Question ques = '3. Will OD be given during the course of the Appathon?' ans='Yes'/>
+        <Question ques= '' ans=''/>
+        <Question ques= '' ans=''/>
+        <Question ques = '' ans=''/>
       </div>
     </section>
   );
